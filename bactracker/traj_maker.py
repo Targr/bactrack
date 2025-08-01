d15c7bbaa00278078c3441dd708e822f983cb842
Targr <btarg888@gmail.com>
2025-08-01 14:55:21 -0500

    Create traj_maker.py

---
+++ bactracker/traj_maker.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def build_trajectories(t_filtered, frame_interval=0.05):
+    traj = []
+    for particle, group in t_filtered.groupby('particle'):
+        traj.append({
+            'particle': particle,
+            'x': group['x'].values,
+            'y': group['y'].values,
+            'time': (group['frame'].values - group['frame'].values[0]) * frame_interval,
+            'dt': np.array([frame_interval])
+        })
+    return traj
