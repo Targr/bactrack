72ca4626efcec538c8550d6fb0efe2362b641da2
Targr <btarg888@gmail.com>
2025-08-04 11:01:15 -0500

    Update __init__.py

--- bactracker/__init__.py
+++ bactracker/__init__.py
@@ -3,3 +3,4 @@ from .detection import merge_rods_by_shape
 from .classify import detect_motility
 from .summarize import summarize_trajectories
 from .interactive import interactive_detection_tuner
+from .traj_maker import build_trajectories
